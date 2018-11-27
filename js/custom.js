5584efd70afecc4ac6e04aba8541ed111785e5ae
JohnWayne22 <zazazaxx100@outlook.com>
2018-11-27 16:47:27 -0600

    Layout final

---
+++ js/custom.js
@@ -0,0 +1,47 @@
+// javascript Animate onscroll Start
+    $(document).ready(function () {
+    if (screen.width > 1024) {
+    AOS.init({
+    easing: 'ease-in-out-sine',
+    once: true,
+    });
+    }
+    });
+
+$('#recipeCarousel').carousel({
+  interval: 5000
+})
+
+$('.carousel').carousel({
+  interval: 5000
+})
+    
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+$('.portfolio .carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
